d1597cd87800f07a7fdd76fae9a7a1d76bca4c82
Alona Sydorova <aln.sydorova@gmail.com>
2024-07-30 20:38:03 +0200

    Fix bugs

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,5 +3,11 @@ import { AUTH_USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: { authorization: localStorage.getItem(AUTH_USER_LOCALSTORAGE_KEY) ?? '' },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(AUTH_USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
