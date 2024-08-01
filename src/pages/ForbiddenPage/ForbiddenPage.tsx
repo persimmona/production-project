473db5ceb3c0e3031ea66dce2d85b8e1b82889c0
Alona Sydorova <aln.sydorova@gmail.com>
2024-08-01 21:23:57 +0200

    Add forbidden page

---
+++ src/pages/ForbiddenPage/ForbiddenPage.tsx
@@ -0,0 +1,15 @@
+import { useTranslation } from 'react-i18next';
+import { Page } from 'shared/ui/Page/Page';
+import { classNames } from 'shared/utils/classNames';
+import cls from './ForbiddenPage.module.scss';
+
+interface ForbiddenPageProps {
+    className?: string;
+}
+
+export function ForbiddenPage(props: ForbiddenPageProps) {
+    const { className } = props;
+    const { t } = useTranslation();
+
+    return <Page className={classNames(cls.forbiddenPage, {}, [className])}>{t('forbidden')}</Page>;
+}
